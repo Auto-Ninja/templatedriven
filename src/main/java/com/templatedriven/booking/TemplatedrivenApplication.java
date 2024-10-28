c620cd876cfce1f35b4c8668dc879d101052b871
SMT <smithajojo@live.com>
2024-10-28 13:13:42 +0000

    Default Template Listening to 8099

---
+++ src/main/java/com/templatedriven/booking/TemplatedrivenApplication.java
@@ -0,0 +1,13 @@
+package com.templatedriven.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplatedrivenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplatedrivenApplication.class, args);
+	}
+
+}
